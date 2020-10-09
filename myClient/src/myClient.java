3a8d40bd2f978fa860916b10aa6a2abe86b4bc4c
Emil Macko <56248702+EmilAceMacko@users.noreply.github.com>
2020-10-09 13:52:00 +0200

    Mathemalicious

--- myClient/src/myClient.java
+++ myClient/src/myClient.java
@@ -1,9 +1,13 @@
+import java.lang.Math;
+
 public class myClient
 {
-
-    public static void main(String[] args) {
-
+    public static void main(String[] args)
+    {
         int num = 3;
         System.out.println("Number = " + num);
+
+        double phi = (1.0 + Math.sqrt(5.0)) / 2.0;
+        System.out.println("Phi is equal to " + phi);
     }
 }
