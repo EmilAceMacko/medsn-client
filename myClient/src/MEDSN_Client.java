9d82e58ac431ecddf975a5a3ea6041aac3e107b1
JeppeP <djjepspage@live.dk>
2020-10-23 13:37:10 +0200

    MEDSN_Client getters and setters

--- myClient/src/MEDSN_Client.java
+++ myClient/src/MEDSN_Client.java
@@ -1,7 +1,49 @@
 package src;
 
+import sun.security.krb5.internal.HostAddress;
+
+import javax.net.ServerSocketFactory;
+import javax.net.ssl.HostnameVerifier;
+
 public class MEDSN_Client implements Constants {
+    public Chat chat;
+    public Server_Manager serverMgr;
+    private Byte state;
+    private String Username;
+    private String hostAddress;
 
     public static void main(String[] args) {
     }
+
+    public byte getState() {
+        return state;
+    }
+
+    public void setState(Byte newState) {
+        this.state = newState;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String newUsername) {
+        this.Username = newUsername;
+    }
+
+    public String getHostAddress(){
+        return hostAddress;
+    }
+
+    public void setHostAddress (String newHostAddress) {
+        this.hostAddress = newHostAddress;
+    }
+
+
+
+
+
+    //setters and getters will be placed under #MyWayOfOptimizingMyWork
+
+
 }
