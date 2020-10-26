a85f814913900b7819e08d81d7f4b06913685001
Toss <56518414+Toss-7055@users.noreply.github.com>
2020-10-26 13:34:56 +0100

    Chat: Minor changes

--- myClient/src/Chat.java
+++ myClient/src/Chat.java
@@ -1,18 +1,18 @@
 package src;
 
-public class Chat
+public class Chat implements Constants
 {
     private MEDSN_Client owner;
 
     public type field;
 
-    public Chat (MEDSN_Client (owner))
+    public Chat (MEDSN_Client owner)
     {
-
+        //
     }
 
     public <type> void Method ()
     {
-
+        //
     }
 }
