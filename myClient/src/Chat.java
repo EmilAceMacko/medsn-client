19706231a321e4688ac16c538cf281340cbba5b5
Toss <56518414+Toss-7055@users.noreply.github.com>
2020-10-29 14:53:22 +0100

    Client Chat.java: has been worked on further

--- myClient/src/Chat.java
+++ myClient/src/Chat.java
@@ -1,24 +1,50 @@
 package src;
 
+import java.util.Scanner;
+
 public class Chat implements Constants
 {
     private MEDSN_Client owner;
-
     public Thread scanThread;
 
-    public Chat (MEDSN_Client owner)
+    public Chat (MEDSN_Client owner) // The Chat's constructor.
     {
-        //
+        scanThread = new Thread() // The inner class for scanThread
+        {
+            private Chat owner;
+            private String input;
+            public Boolean scanning;
+
+            private Scanner scan = new Scanner(System.in);
+
+
+            public scanThread(Chat owner) // A bit of issues with this one. Return to look at the class diagram to fix it later.
+            {
+                //
+            }
+
+            public void run()
+            {
+                while (scanning == true)
+                {
+                    // Check class diagram for further information
+                }
+            }
+
+            public void writeChat(String msg)
+            {
+
+            }
+        };
     }
 
     public String checkInput()
     {
-        return null; //Just for now
+        return null; // Just for now, so it won't complain.
     }
 
     public void writeChat (String msg)
     {
-        //
+        System.out.println(msg);
     }
-
 }
