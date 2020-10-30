d2e51c77660f9eb68797459deeceaf0975aed25f
Toss <56518414+Toss-7055@users.noreply.github.com>
2020-10-30 11:28:17 +0100

    Client Chat.java and ScanThread.java: updated to match class diagram

--- myClient/src/Chat.java
+++ myClient/src/Chat.java
@@ -9,33 +9,7 @@ public class Chat implements Constants
 
     public Chat (MEDSN_Client owner) // The Chat's constructor.
     {
-        scanThread = new Thread() // The inner class for scanThread. Will be moved to the new class for the thread.
-        {
-            private Chat owner;
-            private String input;
-            public Boolean scanning;
-
-            private Scanner scan = new Scanner(System.in);
-
-
-            public ScanThread(Chat owner) // Can't be done like this must make a class for thread in order for it to work.
-            {
-                //
-            }
-
-            public void run()
-            {
-                while (scanning == true)
-                {
-                    // Check class diagram for further information
-                }
-            }
-
-            public void writeChat(String msg)
-            {
-
-            }
-        };
+        //
     }
 
     public String message(String msg)
--- myClient/src/ScanThread.java
+++ myClient/src/ScanThread.java
@@ -1,7 +1,25 @@
 package src;
 
+import java.util.Scanner;
+
 public class ScanThread
 {
     // The new class for the thread used in the Chat class.
 
+    private Chat owner;
+    private String input;
+    public Boolean scanning;
+
+    public ScanThread(Chat owner)
+    {
+        //
+    }
+
+    public void run()
+    {
+        while (scanning == true)
+        {
+            // Check class diagram for further information
+        }
+    }
 }
