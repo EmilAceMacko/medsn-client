a9b63e63f1df7546772cc856927b14607862828e
rmni19 <56552132+rmni19@users.noreply.github.com>
2020-10-30 10:28:40 +0100

    re commit classes

--- myClient/src/Server_Manager.java
+++ myClient/src/Server_Manager.java
@@ -1,5 +1,17 @@
 package src;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Server_Manager {
+    private MEDSN_Client owner;
+    private int port = 8000;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private Socket ServerSocket;
 }
-//yo
