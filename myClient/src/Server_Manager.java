0c12b39f16374c1868e05ace7b41e147d7960e25
rmni19 <56552132+rmni19@users.noreply.github.com>
2020-11-01 17:25:55 +0100

    Server_Manager code

--- myClient/src/Server_Manager.java
+++ myClient/src/Server_Manager.java
@@ -3,9 +3,7 @@ package src;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Server_Manager {
@@ -13,5 +11,99 @@ public class Server_Manager {
     private int port = 8000;
     private DataInputStream in;
     private DataOutputStream out;
-    private Socket ServerSocket;
+    private Socket socket;
+
+    // constructor
+    public Server_Manager(MEDSN_Client owner) {
+        this.owner = owner;
+    }
+
+    public void connect(String host, String name, String pass) {
+
+        try {
+            // attempts to connect to server with socket
+            socket = new Socket(host, port);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            out.writeShort(owner.NET_CLIENT_JOIN_REQUEST);
+            // sends username and admin password to client manager
+            sendMessage(name);
+            sendMessage(pass);
+            // awaits a response from the server
+            receiveMessage();
+        } catch (IOException e) {
+            System.err.println("CLIENT ERROR: " + e + " - Could not set up socket on host " + host + " and port " + port);
+            System.exit(-1); // closes the program
+        }
+
+    }
+    public void sendMessage(String msg){
+        try{
+            // creates byte array from the given message and saves the length
+            byte[] array = msg.getBytes();
+            int length = array.length;
+
+            // writes the identifier, length and array to client manager
+            out.writeShort(owner.NET_CLIENT_CHAT);
+            out.writeInt(length);
+            out.write(array);
+        } catch (IOException e) {
+            System.err.println("CLIENT ERROR: " + e + " - Could not send message");
+            System.exit(-1); // closes the program
+        }
+    }
+    public void receiveMessage(){
+        try{
+            // variables for identifier and connection state for client
+           short clientState = owner.getState();
+           short identifier = in.readShort();
+
+           // checks if the client state is connecting
+           if (clientState == owner.STATE_CLIENT_CONNECTING) {
+
+               String deniedMsg = null;
+                // checks if the server has accepted the connection
+               if (identifier==owner.NET_SERVER_JOIN_ACCEPT){
+                   owner.setState(owner.STATE_CLIENT_ONLINE);
+               }
+               // checks if the server has denied the connection
+               else if (identifier==owner.NET_SERVER_JOIN_DENY_BANNED_IP){
+                   deniedMsg = "Denied: Banned IP";
+               }
+               else if (identifier==owner.NET_SERVER_JOIN_DENY_BANNED_NAME){
+                   deniedMsg = "Denied: Banned Name";
+               }
+               else if (identifier==owner.NET_SERVER_JOIN_DENY_DUPLICATE){
+                   deniedMsg = "Denied: Already connect";
+               }
+               else if (identifier==owner.NET_SERVER_JOIN_DENY_NO_ROOM){
+                   deniedMsg = "Denied: No Room";
+               }
+                // handles denied connection
+               if (deniedMsg != null){
+                   owner.setState(owner.STATE_CLIENT_OFFLINE);
+                   owner.handleChatString(deniedMsg);
+               }
+           }
+           // checks if client state is online
+           else if (clientState == owner.STATE_CLIENT_ONLINE) {
+               // check if message received
+               if (identifier == owner.NET_SERVER_CHAT){
+                   byte[] array = new byte[in.readInt()];
+                   in.read(array);
+                   String msg = Arrays.toString(array);
+                   owner.handleChatString(msg);
+               }
+               // check if client should quit
+               else if (identifier == owner.NET_SERVER_QUIT){
+                   owner.setState(owner.STATE_CLIENT_OFFLINE);
+                   socket.close();
+               }
+           }
+        } catch (IOException e) {
+            System.err.println("CLIENT ERROR: " + e + " - Could not receive message");
+            System.exit(-1); // closes the program
+        }
+    }
 }
+
